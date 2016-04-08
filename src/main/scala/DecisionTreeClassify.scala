ffb9c51c8968d7ff9d223e30177489c28c129606
Sagar Deshpande <deshpandesagar1704@gmail.com>
2016-04-08 01:49:29 -0500

    Done with Decision Tree coding.

---
+++ src/main/scala/DecisionTreeClassify.scala
@@ -0,0 +1,41 @@
+package main.scala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.util.MLUtils
+
+object DecisionTreeClassify {
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf().setAppName("glassClassify")
+        val sc = new SparkContext(conf)
+
+        val input = sc.textFile(args(0))
+        //val input = sc.textFile("S:\\Spring2016\\BigData\\Homeworks\\Homework3\\dataset\\glass.data")
+        input.map(line => line.split(",").tail.reverse.mkString(",")).saveAsTextFile(".\\intermediate")
+
+        val data = MLUtils.loadLabeledPoints(sc, ".\\intermediate")
+        //        val data = MLUtils.loadLabeledPoints(sc, "S:\\Spring2016\\BigData\\Homeworks\\Homework3\\dataset\\glass.data")
+        
+        val splits = data.randomSplit(Array(0.6, 0.4))
+        val (trainingData, testingData) = (splits(0), splits(1))
+
+        val numClasses = 8
+        val categoricalFeaturesInfo = Map[Int, Int]()
+        val impurity = "gini"
+        val maxDepth = 5
+        val maxBins = 32
+
+        val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+            impurity, maxDepth, maxBins)
+
+        val labelPredictions = testingData.map { point =>
+            val prediction = model.predict(point.features)
+            (point.label, prediction)
+        }
+
+        val accuracy = labelPredictions.filter(data => (data._1 == data._2)).count().toDouble / testingData.count()
+
+        println("Decision Tree accuracy:: " + accuracy)
+    }
+}
